e59fac9e1cd4feef618c258f30367e0b643d3a68
joaoalonsobarbosa <153541861+joaoalonsobarbosa@users.noreply.github.com>
2024-06-20 10:14:04 +0100

    Update LoginValidator.java

--- LoginValidator.java
+++ LoginValidator.java
@@ -2,13 +2,4 @@
 public class LoginValidator extends HttpServlet {
 
    
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        
-       
-   
-
-               
-    }
-
 }
