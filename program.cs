28e3383bc887c1753e7e09ccfce80a45dde083a5
João Alonso Barbosa <55247040+joaoalonso8@users.noreply.github.com>
2025-05-27 16:53:37 +0100

    Update program.cs

--- program.cs
+++ program.cs
@@ -7,5 +7,50 @@ using System.Data.SqlClient;
 
 namespace SQLi_1
 {
-    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var user = args[0];
+                var pwd = Encrypt(args[1]);
+                Login(user, pwd);
+            }
+            catch  
+            {
+
+                Console.WriteLine("An error has occurred !!");
+            }
+            
+        }
+
+        private static  string Encrypt(string plain)
+        {
+            return plain;
+        }
+
+        private static void Login(string username,string password)
+        {
+            try
+            {
+                using (var conn = new SqlConnection("conn..."))
+                {
+                    var sql = "SELECT * FROM Users WHERE username = '" + username + "' AND pwd = '" + password + "'";
+                    using (var cmd = new SqlCommand(sql))
+                    {
+                        cmd.Connection = conn;
+                        cmd.ExecuteScalar();
+                    }
+
+                }
+            }
+            catch  
+            {
+
+                Console.WriteLine("An error has occurred !!");
+            }
+           
+        }
+    }
 }
